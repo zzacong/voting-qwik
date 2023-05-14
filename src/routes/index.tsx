4ababcb23ebc095558926321a96c72e0a15bfec2
Zac Ong <61817066+zzacong@users.noreply.github.com>
2023-05-14 17:20:12 +1000

    add signin signout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,16 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
+import { useAuthSession } from '~/routes/plugin@auth';
+
 export default component$(() => {
-  return <></>;
+  const session = useAuthSession();
+
+  return (
+    <>
+      <p>Email: {session.value?.user?.email}</p>
+    </>
+  );
 });
 
 export const head: DocumentHead = {
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -13,6 +13,7 @@ export const useServerTimeLoader = routeLoader$(() => {
 
 export default component$(() => {
   useStyles$(styles);
+
   return (
     <>
       <Header />
